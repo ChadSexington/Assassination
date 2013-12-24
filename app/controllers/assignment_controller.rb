aa56e1161d193fa8f5035a7faa0f707200aba94f
Wallace Daniel <wdaniel@redhat.com>
2013-12-24 10:56:55 -0500

    Added assignment model/controller

---
+++ app/controllers/assignment_controller.rb
@@ -0,0 +1,32 @@
+class AssignmentController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  
+end
