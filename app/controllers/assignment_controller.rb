2fcf0a976112a974f1dee3bce0ffd3d24e6ebfca
Timothy Williams <tiwillia@redhat.com>
2013-12-30 14:06:11 -0500

    Added assignment views and controller methods

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -1,11 +1,28 @@
 class AssignmentController < ApplicationController
   
   def new
-    @assignment = Assignment.new
+    if admin?
+      @assignment = Assignment.new
+    else
+      flash[:error] = "Must be an administrator to create new assignments."
+      redirect_to :back
+    end
   end
   
   def create
-    
+    if admin?
+      @assignment = Assignment.new(assignment_params)
+      if @assignment.save
+        flash[:success] = "Assignment with id " + @assignment.id + " successfully created."
+        redirect_to @assignment
+      else
+        flash[:error] = "Assignment could not be created."
+        redirect_to new
+      end
+    else
+      flash[:error] = "Must be an administrator to create new assignments."
+      redirect_to :back
+    end 
   end
   
   def update
@@ -17,16 +34,20 @@ class AssignmentController < ApplicationController
   end
   
   def index
-    
+   @assignments = Assignment.all 
   end
   
   def show
-    
+    @assignment = Assignment.find(params[:id]) 
   end
   
   def destroy
     
   end
-  
+
+private
+  def assignment_params
+    params.require(:assignment).permit!
+  end
   
 end
