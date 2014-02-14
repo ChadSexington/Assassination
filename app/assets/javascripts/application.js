7f7d72c5e7f4e6cd9483bb362f246d18b30b5958
Wallace <wdaniel@redhat.com>
2014-02-14 17:33:42 -0500

    Gave the admin navbar a facelift and added js code to adjust top padding of body

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+$(document).ready(function() {
+	// find the first container
+	var bodyContainer = $("body").children().eq(1);
+
+	// if an admin is logged in (meaning the second navbar is rendered) adjust the body padding
+	if ($("navbar-inverse") != 0) {
+		bodyContainer.css({"padding-top":"150px"});
+		console.log("Changed the top padding")
+	}
+});
