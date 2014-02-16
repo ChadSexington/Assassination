55fa238b093b24e09b28ac2f5564d6a7bf4c07e2
Timothy Williams <tiwillia@timdev.usersys.redhat.com>
2014-02-16 17:04:25 -0500

    Another migration fix

--- db/migrate/20140213011120_change_target_to_target_id_in_assignments.rb
+++ db/migrate/20140213011120_change_target_to_target_id_in_assignments.rb
@@ -1,6 +1,11 @@
 class ChangeTargetToTargetIdInAssignments < ActiveRecord::Migration
-  def change
+  def up
     remove_column :assignments, :target
     add_column :assignments, :target_id, :integer
+  end
+  
+  def down
+    remove_column :assignments, :target_id
+    add_column :assignments, :target
   end
 end
