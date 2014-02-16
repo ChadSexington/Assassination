c6dc6509f6d725467e276b9dce540849756862fd
Timothy Williams <tiwillia@timdev.usersys.redhat.com>
2014-02-16 17:22:41 -0500

    Another migration fix

--- db/migrate/20140213011119_remove_target_from_assignments.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTargetFromAssignments < ActiveRecord::Migration
-  def up
-    remove_column :assignments, :target
-  end
-  
-  def down
-    add_column :assignments, :target, :integer
-  end
-end
