35cdcfa68d3ad80abf9ae377b71f4e580e0acbe6
Timothy Williams <tiwillia@timdev.usersys.redhat.com>
2014-02-19 23:32:25 -0500

    Email Handler fixes

--- lib/emailhandler.rb
+++ lib/emailhandler.rb
@@ -5,10 +5,9 @@ class EmailHandler
   end
 
   def enqueue(email)
-    puts "Adding #{email.inspect}"
+    Rails.logger.info "Adding email to queue: #{email.inspect}."
     start unless running?
     @queue << email
-    puts "email added, queue: #{@queue.inspect}"
   end
   
   def running?
@@ -17,7 +16,7 @@ class EmailHandler
   
   def start
     @thread = Thread.new do
-      puts "popping queue"
+      Rails.logger.info "Waiting for next email in queue. Current queue: #{@queue.inspect}"
       email = @queue.pop
       puts "Got #{email.inspect} out of queue"
       safe_mail(email[:method_name], email[:args])
@@ -26,25 +25,24 @@ class EmailHandler
   end
 
   def safe_mail(method_name, args)
-      attempts = 1
-      Rails.logger.info "Sending email \"#{method_name}\" with arguments: \"#{args.to_s}\"."
-      begin
-        PlayerMailer.send(method_name, *args).deliver
-      rescue Timeout::Error => e
-        Rails.logger.error "Email timed out on attempt ##{attempts}."
-        Rails.logger.error e.inspect
-        if attempts < 5
-          attempts += 1
-          retry
-        else
-          add({:method_name => method_name, :args => args})
-          Rails.logger.error "Giving up on sending email \"#{method_name}\" after #{attempts} attempts. Adding to queue to attempt at a later time."
-        end
-      rescue => e
-        Rails.logger.error "Email \"#{method_name}\" failed to send due to \"#{e.inspect}\""
+    attempts = 1
+    Rails.logger.info "Sending email \"#{method_name}\" with arguments: \"#{args.to_s}\"."
+    begin
+      PlayerMailer.send(method_name, *args).deliver
+    rescue Timeout::Error => e
+      Rails.logger.error "Email timed out on attempt ##{attempts}."
+      Rails.logger.error e.inspect
+      if attempts < 5
+        attempts += 1
+        retry
+      else
+        enqueue({:method_name => method_name, :args => args})
+        Rails.logger.error "Giving up on sending email \"#{method_name}\" after #{attempts} attempts. Adding to the end of the queue to attempt at a later time."
       end
-  end
- 
+    rescue => e
+      Rails.logger.error "Email \"#{method_name}\" failed to send due to \"#{e.inspect}\""
+    end
+  end 
 
 end
 
