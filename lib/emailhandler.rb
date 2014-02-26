1f8d08c54cb6e8c85d6920d72f7bc663b38760f1
Timothy Williams <tiwillia@timdev.usersys.redhat.com>
2014-02-26 09:03:16 -0500

    Added specific phrase to log entries for easier searching

--- lib/emailhandler.rb
+++ lib/emailhandler.rb
@@ -5,12 +5,12 @@ class EmailHandler
   end
 
   def enqueue(email)
-    Rails.logger.info "Adding email to queue: #{email.inspect}."
+    Rails.logger.info "EMAILHANDLER: Adding email to queue: #{email.inspect}."
     @queue << email
     if running?
-      Rails.logger.info "Thread already started, not starting again"
+      Rails.logger.info "EMAILHANDLER: Thread already started, not starting again"
     else
-      Rails.logger.info "Starting email handler thread..."
+      Rails.logger.info "EMAILHANDLER: Starting email handler thread..."
       start
     end
   end
@@ -34,21 +34,21 @@ class EmailHandler
 
   def safe_mail(method_name, args)
     attempts = 1
-    Rails.logger.info "Sending email \"#{method_name}\" with arguments: \"#{args.to_s}\"."
+    Rails.logger.info "EMAILHANDLER: Sending email \"#{method_name}\" with arguments: \"#{args.to_s}\"."
     begin
       PlayerMailer.send(method_name, *args).deliver
     rescue Timeout::Error => e
-      Rails.logger.error "Email timed out on attempt ##{attempts}."
+      Rails.logger.error "EMAILHANDLER: Email timed out on attempt ##{attempts}."
       Rails.logger.error e.inspect
       if attempts < 2
         attempts += 1
         retry
       else
         enqueue({:method_name => method_name, :args => args})
-        Rails.logger.error "Giving up on sending email \"#{method_name}\" after #{attempts} attempts. Adding to the end of the queue to attempt at a later time."
+        Rails.logger.error "EMAILHANDLER: Giving up on sending email \"#{method_name}\" after #{attempts} attempts. Adding to the end of the queue to attempt at a later time."
       end
     rescue => e
-      Rails.logger.error "Email \"#{method_name}\" failed to send due to \"#{e.inspect}\""
+      Rails.logger.error "EMAILHANDLER: Email \"#{method_name}\" failed to send due to \"#{e.inspect}\""
     end
   end 
 
