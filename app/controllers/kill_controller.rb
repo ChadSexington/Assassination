e4c32cf3294f576d23632ccc81bfcf7c828fce06
Timothy Williams <tiwillia@timdev.usersys.redhat.com>
2014-03-08 12:27:21 -0500

    Handle when a non-active player is reported as killed

--- app/controllers/kill_controller.rb
+++ app/controllers/kill_controller.rb
@@ -3,23 +3,28 @@ class KillController < ApplicationController
   def create
     @kill = Kill.new(kill_params)
     @kill.player_id = current_player.id
-    if @kill.save
-      @death = Death.new(:assassin_id => current_player.id,
-                         :player_id => kill_params[:deceased_id],
-                         :recap => kill_params[:recap],
-                         :location => kill_params[:location])
-      @death.save
+    if not Assignment.where(:target_id => @kill.deceased_id, :active => true).empty?
+      if @kill.save
+        @death = Death.new(:assassin_id => current_player.id,
+                           :player_id => kill_params[:deceased_id],
+                           :recap => kill_params[:recap],
+                           :location => kill_params[:location])
+        @death.save
 
-      assassin_assignment = Player.find(current_player.id).assignments.where(:active => true).last
-      if assassin_assignment.target_id == kill_params[:deceased_id]
-        pass_on_normal_assignment(current_player.id, kill_params[:deceased_id])
+        assassin_assignment = Player.find(current_player.id).assignments.where(:active => true).last
+        if assassin_assignment.target_id == kill_params[:deceased_id]
+          pass_on_normal_assignment(current_player.id, kill_params[:deceased_id])
+        else
+          pass_on_extra_assignment(current_player.id, kill_params[:deceased_id])
+        end
+        flash[:success] = "Kill reported!"
+        redirect_to '/central'
       else
-        pass_on_extra_assignment(current_player.id, kill_params[:deceased_id])
+        flash[:error] = "Kill could not be saved."
+        redirect_to :back
       end
-      flash[:success] = "Kill reported!"
-      redirect_to '/central'
     else
-      flash[:error] = "Kill could not be saved."
+      flash[:error] = "Deceased player is already dead or not playing this round"
       redirect_to :back
     end
   end
