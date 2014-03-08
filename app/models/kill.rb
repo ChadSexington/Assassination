de2f51d16947a0f65c6e9207698af3300be64ec1
Timothy Williams <tiwillia@timdev.usersys.redhat.com>
2014-03-08 13:05:58 -0500

    Enable setting no_display for kills

--- app/models/kill.rb
+++ app/models/kill.rb
@@ -1,4 +1,4 @@
 class Kill < ActiveRecord::Base
-  attr_accessible :deceased_id, :location, :player_id, :recap
+  attr_accessible :deceased_id, :location, :player_id, :recap, :no_display
   belongs_to :player
 end
